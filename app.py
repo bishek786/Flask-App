589c35c38ad8f84d9d96cd34be7cfaf9db2dc6c9
Bishek Yadav 12220081 <115403079+bishek786@users.noreply.github.com>
2024-11-13 11:50:01 +0530

    Update appabc.py
    abc

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, request, jsonify
 from instamojo_wrapper import Instamojo
 from pymongo.mongo_client import MongoClient
+from threading import Thread
 import os
 from dotenv import load_dotenv
 load_dotenv()
-os.getenv("API_KEY")
 
 
 api = Instamojo(api_key=os.getenv("API_KEY"),auth_token=os.getenv("AUTH_TOKEN"))
 app = Flask(__name__)
 
-
+def thread_finc(data1:dict,payment_request):
+    data1.update(payment_request)
+    db.uploadData(data1)
 
 
 class DataBase():
@@ -18,36 +20,25 @@ class DataBase():
         Username = os.getenv("DB_USERNAME")
         Password = os.getenv("DB_PASSWORD")
         self.__uri_db = f"mongodb+srv://{Username}:{Password}@cluster0.rg4pbtc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-        self.__db = False
+        self.client_read = MongoClient(self.__uri_db )
+        user_db = self.client_read["ApnaDB"]
+        self.read_collection = user_db["UserData"]
 
     @property
     def userDB(self):
-        self.client_read = MongoClient(self.__uri_db )
-        self.__db = True
-        user_db = self.client_read["ApnaDB"]
-        read_collection = user_db["UserData"]
-        return read_collection
+        return self.read_collection
 
-    def close(self):
-        if self.__db :
-            self.client_read.close()
-            self.__db = False
 
     def uploadData(self,data:dict):
-        query = {'UniqueCode':data.get('UniqueCode')}
-        update = {'$set': data }
-        result:dict = self.userDB.find_one_and_update(query,update,return_document=True)
-        self.close()
+        if data.get('UniqueCode'):
+            query = {'UniqueCode':data.get('UniqueCode')}
+            update = {'$set': data }
+            result:dict = self.userDB.find_one_and_update(query,update,return_document=False)
         # Check if a document was updated and print it
-        if result:
-            pass
-            # print("Updated document:", result)
-        else:
-            insert_doc = self.userDB.insert_one(data)
-            self.close()
-            # print("No document matched the filter criteria.")
+            if not result:
+                insert_doc = self.userDB.insert_one(data)
 
-
+db = DataBase()
 
 # Create a new Payment
 def createNewPayment()->str:
@@ -71,22 +62,19 @@ def getPaymentStatus(payment_request_id):
 def home():
     return "Welcome to the Home Page!"
 
+
+
 # Define the first route
 @app.route('/Apna-Browser/Initialize-Payment', methods=['POST'])
 def InitializePayment():
-    db = DataBase()
     # # Get JSON data from the incoming request
-    Webhook = None
     data:dict = request.json
+    Webhook = None
     payment_request = createNewPayment()
-    
-    data.update(payment_request)
-    db.uploadData(data)
-    db.close()
-    responseData = getPaymentStatus((payment_request['id']))
+    Webhook = {'longurl':payment_request['longurl'], "payment_request_id":payment_request["id"]}
 
-    if responseData['success']:
-        Webhook = {'shorturl':responseData['payment_request']['shorturl'], "payment_request_id":responseData['payment_request']["id"]}
+    thread1 = Thread(target=thread_finc,args=(data, payment_request))
+    thread1.start()
 
     return jsonify({"success": True, "message": Webhook}), 200
 
@@ -98,7 +86,7 @@ def CompletePayment():
     try:
         data = request.form.to_dict()  # Instamojo typically sends data in form-encoded format
         # Log or process the webhook data as needed
-        print(data)
+        # print(data)
 
         payment_id = data.get('payment_id')
         payment_request_id = data.get('payment_request_id')
@@ -106,12 +94,10 @@ def CompletePayment():
 
         # Process the data based on the payment status
         if status == 'Credit':
-            db = DataBase()
             # Update Data Base Payment is Done
             query = {'id':payment_request_id}
             update = {'$set': data }
             result:dict = db.userDB.find_one_and_update(query,update,return_document=False)
-            db.close()
         else:
             # Handle payment failure or other statuses
             print(f"Payment {payment_id} failed or is pending.")
@@ -124,9 +110,12 @@ def CompletePayment():
         print(f"Error processing webhook: {str(e)}")
         return jsonify({'status': 'error', 'message': str(e)}), 400
 
-    
+
+  
 
 
 # Run the Flask app
 if __name__ == '__main__':
     host = "127.0.0.1"
+    port = 8080
+    app.run(host=host, port=port,debug=False)
