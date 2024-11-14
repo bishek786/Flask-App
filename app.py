626467dffb055a2a0664a68830ad9cb256eddcd8
Bishek Yadav 12220081 <115403079+bishek786@users.noreply.github.com>
2024-11-14 11:34:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,16 @@ load_dotenv()
 api = Instamojo(api_key=os.getenv("API_KEY"),auth_token=os.getenv("AUTH_TOKEN"))
 app = Flask(__name__)
 
-def thread_finc(data1:dict,payment_request):
+def thread_finc(db,data1:dict,payment_request):
     data1.update(payment_request)
     db.uploadData(data1)
+
+
+def thread_finc2(db,data1:dict):
+    payment_request_id = data1.get('payment_request_id') 
+    query = {'id':payment_request_id}
+    update = {'$set': data1}
+    db.userDB.find_one_and_update(query,update,return_document=False)
 
 
 class DataBase():
@@ -36,7 +43,7 @@ class DataBase():
             result:dict = self.userDB.find_one_and_update(query,update,return_document=False)
         # Check if a document was updated and print it
             if not result:
-                insert_doc = self.userDB.insert_one(data)
+                self.userDB.insert_one(data)
 
 db = DataBase()
 
@@ -69,11 +76,10 @@ def home():
 def InitializePayment():
     # # Get JSON data from the incoming request
     data:dict = request.json
-    Webhook = None
     payment_request = createNewPayment()
     Webhook = {'longurl':payment_request['longurl'], "payment_request_id":payment_request["id"]}
 
-    thread1 = Thread(target=thread_finc,args=(data, payment_request))
+    thread1 = Thread(target=thread_finc,args=(db,data, payment_request))
     thread1.start()
 
     return jsonify({"success": True, "message": Webhook}), 200
@@ -86,18 +92,15 @@ def CompletePayment():
     try:
         data = request.form.to_dict()  # Instamojo typically sends data in form-encoded format
         # Log or process the webhook data as needed
-        # print(data)
 
         payment_id = data.get('payment_id')
-        payment_request_id = data.get('payment_request_id')
         status = data.get('status')
 
         # Process the data based on the payment status
         if status == 'Credit':
             # Update Data Base Payment is Done
-            query = {'id':payment_request_id}
-            update = {'$set': data }
-            result:dict = db.userDB.find_one_and_update(query,update,return_document=False)
+            thread2 = Thread(target=thread_finc2,args=(db,data))
+            thread2.start()
         else:
             # Handle payment failure or other statuses
             print(f"Payment {payment_id} failed or is pending.")
@@ -119,3 +122,4 @@ if __name__ == '__main__':
     host = "127.0.0.1"
     port = 8080
     app.run(host=host, port=port,debug=False)
+    
